a8dbcabdd9f0dddce49b3635536ee6bc1ebab710
Jeongmin39 <80705450+Jeongmin39@users.noreply.github.com>
2024-03-28 23:52:14 +0900

    Greedy_Q01

---
+++ CH3_그리디/한정민/Q01_모험가길드.py
@@ -0,0 +1,13 @@
+n = int(input())
+fear = list(map(int, input().split()))
+
+group = 0
+count = 0
+
+fear.sort()
+for i in fear:
+    count += 1
+    if count >= i:
+        group += 1
+        count = 0
+print(group)
