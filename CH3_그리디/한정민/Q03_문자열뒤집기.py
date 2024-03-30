1ed2af2001ab5e0894cc950706a4220e58ee5258
Jeongmin39 <80705450+Jeongmin39@users.noreply.github.com>
2024-03-30 23:32:21 +0900

    Greedy_Q03

---
+++ CH3_그리디/한정민/Q03_문자열뒤집기.py
@@ -0,0 +1,17 @@
+str = input()
+zero_cnt = 0 #0이 연속된 부분
+one_cnt = 0 #1이 연속된 부분
+
+if (str[0] == '0'):
+    zero_cnt += 1
+else:
+    one_cnt += 1
+
+for i in range(len(str) - 1):
+    if (str[i] != str[i+1]):
+        if (str[i] == '0'):
+            zero_cnt += 1
+        else:
+            one_cnt += 1
+
+print(min(zero_cnt, one_cnt))
