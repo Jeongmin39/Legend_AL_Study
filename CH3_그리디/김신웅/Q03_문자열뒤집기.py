c226ebcb1f85ee906d62ce3fcecfc38bc2363488
김신웅 <110598358+Proverbs1603@users.noreply.github.com>
2024-04-03 23:13:16 +0900

    Update Q03_문자열뒤집기.py

--- CH3_그리디/김신웅/Q03_문자열뒤집기.py
+++ CH3_그리디/김신웅/Q03_문자열뒤집기.py
@@ -1,27 +1,27 @@
 number = input()
-num0 = 0
-num1 = 0
-#개수비교하기
-for letter in number:
-    if letter == '0':
-        num0 += 1
-    elif letter == '1':
-        num1 += 1
-        
-#작은거 기준으로 그룹 개수 찾기(그룹 개수가 곧 뒤집기 횟수)
-min_val = '0' if num0 <= num1 else '1'
-groups = 0
-current_group = 0
+
+#그룹수를 각각 세기위해..
+groups_0 = 0
+current_group_0 = 0
+groups_1 = 0
+current_group_1 = 0
 
 for digit in number:
-    if digit == min_val:
-          current_group += 1
-    else:
-        if current_group > 0:
-            groups += 1
-            current_group = 0
+    if digit == '0':
+        current_group_0 += 1
+        if current_group_1 > 0:
+            groups_1 +=1
+            current_group_1 = 0
+    elif digit == '1':
+        current_group_1 += 1
+        if current_group_0 > 0:
+            groups_0 += 1
+            current_group_0 = 0
    
-if current_group > 0:
-    groups += 1
+if current_group_0 > 0:
+    groups_0 += 1
 
-print(groups)
+if current_group_1 > 0:
+    groups_1 += 1
+
+print(min(groups_0, groups_1))
