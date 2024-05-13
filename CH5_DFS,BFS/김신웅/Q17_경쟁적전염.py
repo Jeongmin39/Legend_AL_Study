0715b26be289ff65e3a7272790206deac6977432
김신웅 <110598358+Proverbs1603@users.noreply.github.com>
2024-05-13 11:41:15 +0900

    Q17_경쟁적전염 풀었습니다
    Q17_경쟁적전염 풀었습니다

---
+++ CH5_DFS,BFS/김신웅/Q17_경쟁적전염.py
@@ -0,0 +1,37 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+N, K = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+S, X, Y = map(int, input().split())
+virus = []
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] != 0 :
+            virus.append((graph[i][j], i, j))
+#바이러스를 1,2,3..순서로 정렬
+virus.sort()
+
+#상하좌우
+d = [(-1,0), (1,0), (0,-1), (0,1)]
+
+def bfs(s, X, Y):
+    queue = deque(virus)
+    #while문 도는 횟수가 곧 s초
+    count = 0
+    while queue:
+        if count == s:
+            break
+        for _ in range(len(queue)):
+            prev, x, y = queue.popleft()
+            for i in range(4):
+                nx = x + d[i][0]
+                ny = y + d[i][1]
+                if (0<=nx<N) and (0<=ny<N):
+                    if graph[nx][ny] == 0:
+                        graph[nx][ny] = graph[x][y]
+                        queue.append((graph[nx][ny], nx, ny))
+        count+=1
+    return graph[X-1][Y-1]
+
+print(bfs(S,X,Y))
